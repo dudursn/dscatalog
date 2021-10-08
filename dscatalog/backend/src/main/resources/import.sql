246b9f2266c409c509c9b8f77ec1cbbaf4877929
Eduardo Nascimento <roger.rsn@outlook.com>
2021-10-08 12:33:35 -0300

    Database seeding

---
+++ dscatalog/backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) values ('Eletronics');
+INSERT INTO tb_category (name) values ('Clothes');
+INSERT INTO tb_category (name) values ('Computers');
+INSERT INTO tb_category (name) values ('Books');
