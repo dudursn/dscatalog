137460a40ff37364e05a33b6814f65f5871d15a4
Eduardo Nascimento <roger.rsn@outlook.com>
2023-06-07 15:58:24 -0300

    adding sonar to check code quality

--- dscatalog/backend/src/test/java/io/github/dudursn/dscatalog/tests/controllers/CategoryControllerTest.java
+++ dscatalog/backend/src/test/java/io/github/dudursn/dscatalog/tests/controllers/CategoryControllerTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
-import org.mockito.Mock;
 
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
